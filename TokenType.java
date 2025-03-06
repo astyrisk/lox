c99cd3098fe8b3452fcc696584d039042921939b
astyrisk <marguesto@disroot.org>
2025-03-06 02:04:58 +0100

    second

--- TokenType.java
+++ TokenType.java
@@ -3,7 +3,7 @@ package com.craftinginterpreters.lox;
   enum TokenType {
       // single char token
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE, 
-    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, START,
+    COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
 
       // One or two character tokens.
     BANG, BANG_EQUAL,
